250904c62919bdc71405307e96bb01ce945a32fc
‘zhandehuang’ <859540029@qq.com>
2022-04-24 15:10:45 +0800

    update frontend application

--- vue.config.js
+++ vue.config.js
@@ -54,6 +54,7 @@ module.exports = {
     },
     // 基本路径
     publicPath:  './',
+    productionSourceMap: false,
     // 输出文件目录
     /*outputDir: 'dist', // 默认dist
     // 用于嵌套生成的静态资产（js,css,img,fonts）目录
